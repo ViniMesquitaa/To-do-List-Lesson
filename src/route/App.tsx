3f05c627f4d3046f6d6cad8b5e5bee57591e4c5b
Vinicius <vinniciussramallho98@gmail.com>
2025-07-26 15:32:53 -0300

    route: under construction page

--- src/route/App.tsx
+++ src/route/App.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import TodoPage from "../pages/TodoPage";
+import LoginPage from "../pages/LoginPage";
+import UnderConstruction from "../components/shared/UnderConstruction";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<TodoPage />} />
+      <Route path="/todo" element={<TodoPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/building" element={<UnderConstruction />} />
     </Routes>
   );
 }
