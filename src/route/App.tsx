055ba46a8f715b35b82790fd75d1389fec37a419
Vinicius <vinniciussramallho98@gmail.com>
2025-07-26 15:52:51 -0300

    feat: replace to login

--- src/route/App.tsx
+++ src/route/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import TodoPage from "../pages/TodoPage";
 import LoginPage from "../pages/LoginPage";
 import UnderConstruction from "../components/shared/UnderConstruction";
@@ -6,6 +6,7 @@ import UnderConstruction from "../components/shared/UnderConstruction";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/todo" element={<TodoPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/building" element={<UnderConstruction />} />
